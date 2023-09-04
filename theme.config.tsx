92bb9cb42ded712b2331639f819d8d8553739a0b
kasuie <chenkauie@gmail.com>
2023-09-04 15:11:59 +0800

    fix:页面信息修改

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+/*
+ * @Author: kasuie
+ * @Date: 2023-09-04 15:01:26
+ * @LastEditors: kasuie
+ * @LastEditTime: 2023-09-04 15:10:51
+ * @Description:
+ */
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>KasuieのDocs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/kasuie",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "文档中心",
   },
-}
+};
 
-export default config
+export default config;
