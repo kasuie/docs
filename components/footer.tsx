276c9357e8a7d58692a9b6746531d8b809cb1acc
kasuie <chenkauie@gmail.com>
2023-09-05 17:39:40 +0800

    fix:一些优化

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,15 @@
  * @Author: kasuie
  * @Date: 2023-09-05 14:00:45
  * @LastEditors: kasuie
- * @LastEditTime: 2023-09-05 14:03:13
+ * @LastEditTime: 2023-09-05 17:37:36
  * @Description: 
  */
 import styles from "./footer.module.css";
 
 export default function Footer({ text }) {
-  return <div className={styles.footer}>
+  return <div className={styles.footer} onClick={() => {
+    window.open("https://index.kasuie.cc", "_blank");
+  }}>
     {text}
   </div>;
 }
