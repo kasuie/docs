82cbff1905a3ec94e9e6c3d8ea0b843e65a4ea2f
kasuie <chenkauie@gmail.com>
2024-05-22 11:10:03 +0800

    feat: ✨icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -2,42 +2,48 @@
  * @Author: kasuie
  * @Date: 2024-05-21 20:01:07
  * @LastEditors: kasuie
- * @LastEditTime: 2024-05-21 21:24:19
+ * @LastEditTime: 2024-05-22 10:56:45
  * @Description:
  */
 import { useEffect, useState } from "react";
-import { Icons as GetIcons } from "@kasuie/icon";
+import { Icons as KIcons, IconNames } from "@kasuie/icon";
+import { copyToClipboard } from "@kasuie/utils";
 
 export default function Icons() {
   const [icons, setIcons] = useState();
   const [iconsCom, setIconsCom] = useState();
 
   useEffect(() => {
-    const myIcons = GetIcons();
-    if (!icons && myIcons) setIcons(myIcons);
-    onLoad();
+    if (!icons && KIcons) setIcons(IconNames());
+    if (!iconsCom && KIcons) setIconsCom(KIcons());
   }, []);
 
-  const onLoad = async () => {
-    if (icons) {
-      const module = await import("@kasuie/icon");
-      console.log(module);
-      setIconsCom(module);
-    }
-  };
-
   const onIcon = (iconName: any) => {
-    console.log(iconName, iconsCom);
-
-    // const IconCom: any = iconsCom[iconName];
-    return <div>555</div>;
+    const IconCom = iconsCom[iconName];
+    return IconCom ? <IconCom size={24} /> : null;
   };
 
   return (
-    <ul>
+    <ul
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "16px",
+        padding: "12px",
+      }}
+    >
       {icons?.map((iconName) => {
         return (
-          <li key={iconName}>
+          <li
+            key={iconName}
+            style={{
+              cursor: "pointer",
+            }}
+            title={`复制${iconName}`}
+            onClick={() => {
+              copyToClipboard(`<${iconName} />`);
+            }}
+          >
             <span>{onIcon(iconName)}</span>
           </li>
         );
